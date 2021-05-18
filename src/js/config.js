e1eaf02d20a9f300db0c82b194e2b8001f2d6aa7
Tyler <tylerfoger@gmail.com>
2021-05-18 14:03:56 -0600

    Changed target url from direct link to localhost

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const url = `http://ec2-34-214-41-107.us-west-2.compute.amazonaws.com:5000/cave`;
+export const url = `http://localhost:5000/cave`;
 export const setupGame = {
   choice: 'default',
   looked: false,
