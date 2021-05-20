f3f20b999dc3610f6c3a33ae3127c8111714fc51
Tyler <tylerfoger@gmail.com>
2021-05-20 14:41:33 -0600

    Localhost in the URL refers to the clients local machine instead of the 'app''s local machine, so that doesn't work. Adding in the address to the AWS location and my local server to make it easier to swap

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,8 @@
-export const url = `http://localhost:5000/cave`;
+// This one is for the AWS server:
+export const url = `http://ec2-34-214-41-107.us-west-2.compute.amazonaws.com:5000/cave`;
+
+// this one for my local Dev server:
+// export const url = `http://192.168.1.196:5000/cave`;
 export const setupGame = {
   choice: 'default',
   looked: false,
