289a54b13a6f18a6ac270aacbce747285fca8a18
Tyler Fougere <tylerfoger@gmail.com>
2023-05-27 15:07:30 -0600

    pre reimage

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,4 @@
-// This one is for the AWS server:
-export const url = `http://ec2-34-214-41-107.us-west-2.compute.amazonaws.com:5000/cave`;
-
-// this one for my local Dev server:
-// export const url = `http://192.168.1.196:5000/cave`;
+export const url = `http://127.0.0.1:5000/cave`;
 export const setupGame = {
   choice: 'default',
   looked: false,
